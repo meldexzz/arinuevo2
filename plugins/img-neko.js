c771c270fa25188e323b44b06aac1815f3a7dcd6
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-22 18:50:48 -0600

    Update img-neko.js

--- plugins/img-neko.js
+++ plugins/img-neko.js
@@ -12,6 +12,6 @@ await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', null, m)
 handler.help = ['neko']
 handler.tags = ['img']
 handler.command = ['neko']
-handler.register = true 
+handler.register = false 
 //handler.limit = 1
 export default handler
