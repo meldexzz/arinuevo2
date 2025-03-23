04347871c47f6ad56703640fd5486005daeb1cf9
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-22 18:54:54 -0600

    Update rpg-claim.js

--- plugins/rpg-claim.js
+++ plugins/rpg-claim.js
@@ -17,7 +17,7 @@ let handler = async (m) => {
 handler.help = ['claim']
 handler.tags = ['rpg']
 handler.command = ['daily', 'claim']
-handler.register = true 
+handler.register = false 
 
 export default handler
 
