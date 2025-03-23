2b5bad8ca553469e60d0178d17dd8f4af30f989e
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-22 18:52:10 -0600

    Update main-infototal.js

--- plugins/main-infototal.js
+++ plugins/main-infototal.js
@@ -8,5 +8,5 @@ conn.reply(m.chat, `*🍭 Total de Funciones* : ${totalf}`,m)
 handler.help = ['totalfunciones']
 handler.tags = ['main']
 handler.command = ['totalfunciones']
-handler.register = true
+handler.register = false
 export default handler 
