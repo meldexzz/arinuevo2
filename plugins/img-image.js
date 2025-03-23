28dc5e184f6495983e5935e58a7436ac0e62bbed
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-22 18:50:26 -0600

    Update img-image.js

--- plugins/img-image.js
+++ plugins/img-image.js
@@ -1,7 +1,7 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Sumi Sakurasawa Icons`)
+if (!text) return m.reply('🍭 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ari Icons`)
 try {
 let { dl_url } = await Scraper.GoogleImage(text)
 await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
@@ -10,6 +10,6 @@ await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
 handler.help = ['imagen <búsqueda>']
 handler.tags = ['img']
 handler.command = ['image', 'gimage', 'imagen']
-handler.register = true 
+handler.register = false 
 //handler.limit = 1
 export default handler
