bff463297e52703ee008c8d93c65bfd991e8d1ae
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-22 19:01:01 -0600

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -21,5 +21,5 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['wm <nombre>|<autor>']
 handler.tags = ['sticker']
 handler.command = ['take', 'robar', 'wm'] 
-handler.register = true 
+handler.register = false 
 export default handler
