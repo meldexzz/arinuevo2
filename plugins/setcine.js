f738fd454ed24937cc2d2205699f20333bf94210
meldexzz <140272375+meldexzz@users.noreply.github.com>
2025-03-26 10:46:51 -0600

    Create setcine.js

---
+++ plugins/setcine.js
@@ -0,0 +1,13 @@
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+    if (text) {
+        global.db.data.chats[m.chat].setcine = text; // Guardamos el set de Cine
+        conn.reply(m.chat, '𝙇𝙤𝙨 𝙘𝙤𝙢𝙗𝙤𝙨 𝙙𝙚 𝘾𝙞𝙣𝙚 𝙝𝙖𝙣 𝙨𝙞𝙙𝙤 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙤𝙨.', m);
+    } else {
+        throw `𝙀𝙨𝙘𝙧𝙞𝙗𝙚 𝙡𝙤𝙨 𝙘𝙤𝙢𝙗𝙤𝙨 𝙦𝙪𝙚 𝙙𝙚𝙨𝙚𝙖𝙨 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙚𝙣 𝙚𝙨𝙩𝙚 𝙜𝙧𝙪𝙥𝙤, 𝙚𝙟𝙚𝙢𝙥𝙡𝙤: \n.setcine 𝙇𝙤𝙨 𝙢𝙚𝙟𝙤𝙧𝙚𝙨 𝙡𝙤𝙠𝙤𝙣𝙤𝙨.`;
+    }
+};
+
+handler.command = ['setcine']; // Comando de Cine
+handler.admin = true;
+handler.group = true;
+export default handler;
